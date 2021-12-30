b7114575e408d0fd6dc39672124a90359e1121f2
Yeaseen Ahmed <92775489+yeaseen-564@users.noreply.github.com>
2021-12-30 22:45:35 +0600

    Update red.dart

--- red.dart
+++ red.dart
@@ -1,1 +1,17 @@
 
+import 'package:flutter/material.dart';
+
+class Red extends StatelessWidget {
+  const Red({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.red,
+      ),
+    );
+  }
+}
